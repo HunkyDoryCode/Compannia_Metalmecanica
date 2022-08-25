f733ff0df89d2189f82260a44a1e6db1f0950d81
HunkyDoryCode <hunkydorycode@gmail.com>
2022-08-25 18:59:27 -0500

    Se actualiza la clase Empleado en el repositorio

--- Empleado/src/main/java/com/HonkyDoryCode/Empleado/Empleado.java
+++ Empleado/src/main/java/com/HonkyDoryCode/Empleado/Empleado.java
@@ -1,2 +1,55 @@
-package com.HonkyDoryCode.Empleado;public class Empleado {
+package com.HonkyDoryCode.Empleado;
+
+public class Empleado {
+    import java.util.Scanner;
+
+    class Empleado {
+        private String Nombres;
+        private String Correo;
+        private String Empresa;
+        private String cargo;
+
+        //Constructor
+
+        public Empleado(String nombres, String correo, String empresa, String cargo) {
+            Nombres = nombres;
+            Correo = correo;
+            Empresa = empresa;
+            this.cargo = cargo;
+        }
+
+    }
+
+    //Getters and Setters
+    public String getNombres() {
+        return Nombres;
+    }
+
+    public void setNombres(String nombres) {
+        Nombres = nombres;
+    }
+
+    public String getCorreo() {
+        return Correo;
+    }
+
+    public void setCorreo(String correo) {
+        Correo = correo;
+    }
+
+    public String getEmpresa() {
+        return Empresa;
+    }
+
+    public void setEmpresa(String empresa) {
+        Empresa = empresa;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
 }
